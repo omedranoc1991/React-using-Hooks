18447e4144669f6a761f0b1ded14c683de4e95b5
Oscar Medrano Castillo <omedranoc@hotmail.com>
2020-07-15 16:32:20 -0500

    I added an advance of our first project using react hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Accordion from './components/Accordion'
+import Search from './components/Search'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const items = [
+    {title : 'What is React', content : 'React is a java script framework'},
+    {title : 'Why use React', content : 'React is a favorite JS library among engeniers'},
+    {title : 'How do you use React', content : 'You use React by creating components'}
+]
+
+const App = () =>{
+    return(
+      <div><Search /></div>
+    ) 
 }
 
-export default App;
+export default App
