3bb78ceedf2c57c2e3e551d38665ad7a1579cf64
Oscar Medrano Castillo <omedranoc@hotmail.com>
2020-07-20 19:04:18 -0500

    an  dropdown with hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Accordion from './components/Accordion'
 import Search from './components/Search'
+import Dropdown from './components/Dropdown'
 
 const items = [
     {title : 'What is React', content : 'React is a java script framework'},
@@ -8,9 +9,33 @@ const items = [
     {title : 'How do you use React', content : 'You use React by creating components'}
 ]
 
+const options = [
+  {
+   label : 'the color red',
+   value : 'red'
+  },
+  {
+    label : 'the color green',
+    value: 'green'
+  },
+  {
+    label: 'A shade of blue',
+    value: 'blue'
+  }
+]
+
 const App = () =>{
+
+  const [selected, setSelected] = useState(options[0])
+
     return(
-      <div><Search /></div>
+      <div>
+        <Dropdown 
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+        />       
+      </div>
     ) 
 }
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -21,13 +21,22 @@ const Search = ()=>{
         })
         setResults(data.query.search)
     }
-    
-    const timeoutId = setTimeout(()=>{
-        if(term){
-            search()
-        }  
 
-    }, 500)    
+    if(term && !results.length){
+        search()
+    }else{
+        const timeoutId = setTimeout(()=>{
+            if(term){
+                search()
+            }  
+    
+        }, 1000)  
+        
+        return ()=>{clearTimeout(timeoutId)}
+        
+    }
+    
+    
 
     },[term])
 
